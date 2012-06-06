c63e9b0f9a64fdb8091be78e3c4a6619f872a5f3
Jean-Mark Wright <jmwright@monainformatixltd.com>
2012-06-06 07:59:13 -0500

    Added index view in views.py

--- views.py
+++ views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Chicem v1")
+    return render_to_response("index.html")
 

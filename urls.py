14d3de5c97817ea0481b5caaf9793191b0014831
Jean-Mark Wright <jmwright@monainformatixltd.com>
2012-06-06 04:55:54 -0500

    added in include for urls from graves app and enabled admin

--- urls.py
+++ urls.py
@@ -5,9 +5,8 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'chicem.views.home', name='home'),
-    # url(r'^chicem/', include('chicem.foo.urls')),
+    url(r'^$', 'chicem.views.home', name='home'),
+    url(r'^graves/', include('chicem.graves.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),

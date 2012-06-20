9f981d655ab7aed9b58ccc4fcbf23128cf0ac478
Jean-Paul Rose <jprose@monainformatixltd.com>
2012-06-19 20:19:03 -0500

    Added url view for map as well as json format output for the graves data. Generated a requirements.txt file for the project

--- graves/views.py
+++ graves/views.py
@@ -2,15 +2,47 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from graves.models import Grave, CemeteryBoundary
 from django.conf import settings
+import json
 
 
-def data(request):
+def data(request, format=None):
     """ returns the data settings in XML for ammap (graves and areas)
     """
-    graves = Grave.objects.all()
-    boundaries = CemeteryBoundary.objects.all()
-    swf_location = "ammap.swf"
-    return render_to_response("graves/data.xml", locals())
+    if format == 'json':
+        print 'creating json version of data'
+        # TODO: need to replace this with a template!
+        results =  {
+            'areas': [],
+            'movies': []
+        }
+
+        boundaries = CemeteryBoundary.objects.all()
+        for boundary in boundaries:
+            new_area = {
+                'instanceName': boundary.section,
+            }
+            results['areas'].append(new_area)
+
+        # for each movie we need zoom_level, title and lat-lng
+        graves = Grave.objects.all()
+        for grave in graves:
+            new_movie = {
+                'zoomLevel': 500,  # not sure yet what this needs to be
+                'title': grave.name,
+                'lat': grave.geom.centroid.y,
+                'lon': grave.geom.centroid.x,
+            }
+            
+            results['movies'].append(new_movie)
+
+        json_data = json.dumps(results)
+        return HttpResponse(json_data, mimetype='application/json')
+
+    else:
+        graves = Grave.objects.all()
+        boundaries = CemeteryBoundary.objects.all()
+        swf_location = "ammap.swf"
+        return render_to_response("graves/data.xml", locals())
 
 
 def search(request, query):
--- views.py
+++ views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Chicem v1")
 
+
+def map(request):
+	return render_to_response('map/ChineseCemetery.html', locals())
